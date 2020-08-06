6cc8d75896f3aed628a22de364924a80a73e5d28
dave <jezreldavesondia@gmail.com>
2020-08-06 20:02:36 +0800

    8-6

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import 'babel-polyfill'
+import "babel-polyfill";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
