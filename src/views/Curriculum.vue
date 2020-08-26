c090fa722d48e693c05fdef4fe876bdb0c6dd427
dave <jezreldavesondia@gmail.com>
2020-08-26 17:50:47 +0800

    8-26

--- src/views/Curriculum.vue
+++ src/views/Curriculum.vue
@@ -144,7 +144,7 @@
               </v-slide-group>
             </v-sheet>
 
-            <div
+            <!-- <div
               class="text-purple headline font-weight-bold text-center mt-10"
             >“짜임새 있는, 기초부터 탄탄한영어를 만듭니다.”</div>
             <div
@@ -175,246 +175,13 @@
                   style="letter-spacing:-1px !important"
                 >택배 or 별도의 출력 없이 교재를 다운로드 하실 수 있습니다.</div>
               </v-col>
-            </v-row>
-            <v-row justify="center" class="my-10">
-              <v-card flat class="rounded-xl">
-                <v-simple-table style="width:100%">
-                  <template v-slot:default>
-                    <thead class="rounded-xl">
-                      <tr
-                        class="rounded-xl"
-                        style="background: linear-gradient(to right, #83b7d3, #725ce0); border-radius: 25px"
-                      >
-                        <th style="background: #bed6e94F"></th>
-                        <th colspan="3" class="text-center white--text">Level 1 ~ 3</th>
-                        <th colspan="2" class="text-center white--text">Level 4 ~ 5</th>
-                        <th colspan="2" class="text-center white--text">Level 6 ~ 7</th>
-                        <th colspan="2" class="text-center white--text">Level 8 ~ 9</th>
-                        <th class="text-center white--text">Level 10</th>
-                      </tr>
-                    </thead>
-                    <tbody class="text-center">
-                      <tr>
-                        <td>입문과정</td>
-                        <td>
-                          기초
-                          <br />발음교정
-                        </td>
-                        <td>
-                          기초
-                          <br />문장연습
-                        </td>
-                        <td>
-                          기초
-                          <br />회화연습
-                        </td>
-                        <td colspan="7"></td>
-                      </tr>
-                      <tr>
-                        <td>
-                          초/ 중급
-                          <br />회화과정
-                        </td>
-                        <td></td>
-                        <td></td>
-                        <td>초급회화</td>
-                        <td>중급회화</td>
-                        <td colspan="6"></td>
-                      </tr>
-                      <!-- <tr>
-                        <td>
-                          정규
-                          <br />회화과정
-                        </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>정규회화</td>
-                        <td colspan="5"></td>
-                      </tr>-->
-                      <tr>
-                        <td>
-                          뉴욕라이브
-                          <br />잉글리쉬
-                        </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                          뉴욕라이브
-                          <br />잉글리쉬
-                        </td>
-                        <td colspan="4"></td>
-                      </tr>
-                      <tr>
-                        <td>
-                          문법
-                          <br />패턴과정
-                        </td>
-                        <td>기초 패턴회화</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>문법 패턴회화</td>
-                        <td colspan="4"></td>
-                      </tr>
-                      <tr>
-                        <td>
-                          토론/
-                          <br />프리토킹
-                        </td>
-                        <td>초급 토론과정</td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                          중급
-                          <br />토론과정
-                        </td>
-                        <td>
-                          심화토론
-                          <br />과정고급
-                        </td>
-                        <td>
-                          시사
-                          <br />토론과정
-                        </td>
-                        <td colspan="4"></td>
-                      </tr>
-                      <tr>
-                        <td>
-                          영자신문
-                          <br />과정
-                        </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                          주니어
-                          <br />조선 초급
-                        </td>
-                        <td>
-                          주니어
-                          <br />조선 중급
-                        </td>
-                        <td>
-                          주니어
-                          <br />조선 고급
-                        </td>
-                        <td>
-                          전문조선
-                          <br />초급
-                        </td>
-                        <td>
-                          전문조선
-                          <br />고급
-                        </td>
-                        <td>
-                          전문조선
-                          <br />고급
-                        </td>
-                        <td colspan="1"></td>
-                      </tr>
-                      <tr>
-                        <td>
-                          비즈니스
-                          <br />과정
-                        </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                          비즈니스
-                          <br />회화
-                        </td>
-                        <td>
-                          비즈니스
-                          <br />맞춤
-                        </td>
-                        <td></td>
-                        <td>
-                          비즈니스
-                          <br />토론
-                        </td>
-                        <td colspan="2"></td>
-                      </tr>
-                      <tr>
-                        <td>
-                          다락원
-                          <br />교재 과정
-                        </td>
-                        <td>
-                          패턴
-                          <br />연습편1
-                        </td>
-                        <td>
-                          패턴
-                          <br />연습편2
-                        </td>
-                        <td>
-                          패턴연습편3
-                          <br />동사연습편1
-                          <br />상황별연습1
-                        </td>
-                        <td>
-                          동사연습편2
-                          <br />상황별연습2
-                          <br />비즈니스전화
-                        </td>
-                        <td>
-                          동사연습편3
-                          <br />상황별연습3
-                          <br />비즈니스회의
-                        </td>
-                        <td>
-                          비즈니스
-                          <br />이메일
-                        </td>
-                        <td>
-                          비즈니스
-                          <br />프리젠테이션
-                        </td>
-                        <td colspan="3"></td>
-                      </tr>
-                      <tr>
-                        <td>
-                          특별전문/
-                          <br />취업준비과정
-                        </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>취업 인터뷰</td>
-                        <td>
-                          호텔영어
-                          <br />승무원영어
-                        </td>
-                        <td>
-                          IELTS
-                          <br />오픽
-                        </td>
-                        <td>
-                          토익스피킹
-                          <br />심화토익스피킹
-                        </td>
-                        <td>의학영어</td>
-                      </tr>
-                    </tbody>
-                  </template>
-                </v-simple-table>
-              </v-card>
-            </v-row>
+            </v-row>-->
+
             <div class="mt-10 pt-10 h4 nanum text-purple font-weight-bold">
               {{category}}
-              <span class="h6 text-purple font-weight-bold">기초발음교정 (level 1~3)</span>
+              <!-- <span class="h6 text-purple font-weight-bold">기초발음교정 (level 1~3)</span> -->
             </div>
-            <div class="d-flex justify-center">
+            <div class="d-flex justify-center mb-10">
               <v-card
                 flat
                 color="#fafafa"
@@ -431,7 +198,12 @@
             <v-sheet color="#fafafa">
               <v-slide-group v-model="bookSlide0" v-if="slide == 0" show-arrows class="mb-3">
                 <v-slide-item class="pa-3" v-for="(book, i) in allBooks.books_1" :key="i">
-                  <v-card flat color="#fafafa">
+                  <v-card
+                    flat
+                    color="#fafafa"
+                    class="rounded-xl mx-2"
+                    style="border: 1px solid skyblue"
+                  >
                     <v-card flat max-width="300" color="#ebebeb" class="ma-3 py-5">
                       <v-img :src="book.image" width="80%" class="mx-auto"></v-img>
                     </v-card>
