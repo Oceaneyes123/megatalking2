d41c6e2663bd849b93141021d0afa5c1d0660cec
dave <jezreldavesondia@gmail.com>
2020-11-25 12:21:23 +0800

    11-25

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -40,23 +40,96 @@
                       <td>{{ item.method }}</td>
                       <td>{{ item.status }}</td>
                       <td>
-                        <v-btn
-                          class="white--text rounded-pill"
-                          style="
-                            background: linear-gradient(
-                              to right,
-                              #8fa1fe,
-                              #4d94e9
-                            );
-                          "
-                          >영수증 | 수강증 | 출석증명서</v-btn
-                        >
+                        <div>
+                          <v-btn
+                            class="white--text rounded-pill"
+                            depressed
+                            style="
+                              background: linear-gradient(
+                                to right,
+                                #8fa1fe,
+                                #4d94e9
+                              ) !important;
+                            "
+                            @click="certificateDialog = true"
+                            >영수증 | 수강증 | 출석증명서</v-btn
+                          >
+                        </div>
                       </td>
                     </tr>
                   </tbody>
                 </template>
               </v-simple-table>
             </v-card>
+
+            <v-dialog
+              max-width="500"
+              v-model="certificateDialog"
+              class="rounded-xl"
+            >
+              <v-card max-width="500" class="rounded-xl">
+                <v-card
+                  color="#6a9af2"
+                  class="pa-3 white--text font-weight-bold h6 rounded-tr-xl rounded-tl-xl"
+                  tile
+                >
+                  Receipt
+                </v-card>
+                <v-container class="pa-3">
+                  <v-row no-gutters class="mb-2">
+                    <v-col cols="3" class="font-weight-bold">Name</v-col>
+                    <v-col cols="9">[Name]</v-col>
+                  </v-row>
+                  <v-row no-gutters class="mb-2">
+                    <v-col cols="3" class="font-weight-bold">Course</v-col>
+                    <v-col cols="9">[Course]</v-col>
+                  </v-row>
+                  <v-row no-gutters class="mb-2">
+                    <v-col cols="3" class="font-weight-bold">Contract</v-col>
+                    <v-col cols="9">[Contract]</v-col>
+                  </v-row>
+                  <v-row no-gutters class="mb-2">
+                    <v-col cols="3" class="font-weight-bold">Period</v-col>
+                    <v-col cols="9">[Period]</v-col>
+                  </v-row>
+                  <v-row no-gutters class="mb-2">
+                    <v-col cols="3" class="font-weight-bold">Start Date</v-col>
+                    <v-col cols="9">[Start Date]</v-col>
+                  </v-row>
+                  <v-row no-gutters class="mb-2">
+                    <v-col cols="3" class="font-weight-bold">Instructor</v-col>
+                    <v-col cols="9">[Instructor]</v-col>
+                  </v-row>
+                  <v-row no-gutters class="mb-5">
+                    <v-col cols="3" class="font-weight-bold">Tuition</v-col>
+                    <v-col cols="9">[Tuition]</v-col>
+                  </v-row>
+
+                  <v-row no-gutters>
+                    <v-col cols="10 caption-text">
+                      The member above proves that they have taken the
+                      MegaTalking English class as above.
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col>
+                      <v-img
+                        src="../assets/mega_logo.png"
+                        max-width="60%"
+                      ></v-img>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="d-flex justify-end">
+                      <v-btn class="mr-3" outlined color="#6a9af2">
+                        Cancel
+                      </v-btn>
+                      <v-btn color="#6a9af2" class="white--text"> Print </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card>
+            </v-dialog>
           </v-container>
         </v-tab-item>
         <v-tab-item>
@@ -265,6 +338,7 @@ export default {
       rating: 4.8,
       screenWidth: "",
       isMobile: false,
+      certificateDialog: false,
       tabs: ["수강내역", "적립금", "할인쿠폰", "회원정보수정"],
       tab1Header: [
         "No",
