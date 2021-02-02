034d6a517b94b3ebedbb691a959d8754bc664828
unknown <jezreldavesondia@gmail.com>
2021-02-02 19:25:16 +0800

    2-2 Board UI

--- src/components/BoardDialog.vue
+++ src/components/BoardDialog.vue
@@ -2,27 +2,25 @@
   <v-dialog v-model="boardDialog" width="700">
     <v-card color="#A9A7F3" dark class="rounded-xl">
       <v-card-title>
-        <span class="h5 font-weight-black nanum">{{ title }}</span>
+        <span class="h5 font-weight-black nanum">Class Review</span>
       </v-card-title>
       <v-card-text class="white text--primary">
-        <p
-          class="h6 font-weight-bold nanum pt-5 text-right"
-          style="color: #6a6a6a"
-        >
-          {{ date }}
-        </p>
-        <v-container>
-          <v-row>
-            <v-col cols="6" class="d-flex justify-center">
-              <v-icon color="#000" style="font-size:150px"
-                >fas fa-chalkboard-teacher</v-icon
-              >
-            </v-col>
-            <v-col cols="6">
-              <div class="h4 nanum mb-5">{{ course }}</div>
-              <div class="h6 " style="color:#A9A7F3">{{ member_name }}</div>
-            </v-col>
-          </v-row>
+        <div class=" nanum pt-5" style="color: #6a6a6a">
+          <div class="d-flex">
+            <div class="h6 font-weight-bold mb-3">{{ title }}</div>
+            <v-spacer></v-spacer>
+            <div class="text-right">{{ date }}</div>
+          </div>
+        </div>
+        <div class="h6 " style="color:#A9A7F3">{{ member_name }}</div>
+        <v-container fluid class="px-0 my-5">
+          <div>
+            Lorem ipsum dolor sit amet, sit liber deleniti maluisset te. Te
+            apeirian facilisi insolens cum, mea an alienum incorrupte. Nec id
+            decore delectus evertitur, postea feugiat eam ne, vulputate
+            constituam ea sea. An malis hendrerit expetendis mel, ei quod ipsum
+            suavitate sea, id quodsi eruditi delenit vix.
+          </div>
         </v-container>
         <v-container>
           <v-row class="justify-end">
