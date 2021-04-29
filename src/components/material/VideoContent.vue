71e622eae1d123fde1e40f12018f9eb6c6daa9ce
unknown <jezreldavesondia@gmail.com>
2021-04-29 11:08:30 +0800

    4-29

--- src/components/material/VideoContent.vue
+++ src/components/material/VideoContent.vue
@@ -8,7 +8,7 @@ export default {
   computed: {
     link() {
       // return `https://megatalking.co.kr/students-material/#/player?unitid=${this.unitId}`;
-      return `https://ueducms.org/students-material/#/player?unitid=${this.unitId}`;
+      return `https://ueducms.org/students-material/dist/#/player?unitid=${this.unitId}`;
     }
   }
 };
